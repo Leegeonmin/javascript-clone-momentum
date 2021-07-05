baf70e24f7fb9eb38dc6d7be64d75c100ded2323
LeeGeonMin <kmlee97s@gmail.com>
2021-07-06 01:28:35 +0900

    finish js

--- js/weather.js
+++ js/weather.js
@@ -6,7 +6,14 @@ function success(loc){
     const longt = loc.coords.longitude;
 
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${longt}&appid=${API_KEY}`;
-    fetch(url);
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        const city = document.querySelector("#weather span:first-child");
+        const weather = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `/ ${data.weather[0].main}`;
+    });
 }
 
 function fail(loc){
